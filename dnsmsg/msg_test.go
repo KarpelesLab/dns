96f1a6fc2c83ff982d2936df938f13804783e3c6
Mark Karpeles <magicaltux@gmail.com>
2020-04-13 01:57:45 +0900

    test response too

--- dnsmsg/msg_test.go
+++ dnsmsg/msg_test.go
@@ -21,4 +21,19 @@ func TestParse(t *testing.T) {
 	}
 
 	log.Printf("parsed: %s", msg.String())
+
+	hexB = "236f8180000100010000000106676f6f676c6503636f6d0000010001c00c00010001000000cd0004acd9af6e0000290200000000000000"
+	b, _ = hex.DecodeString(hexB)
+
+	// parse
+	msg, err = Parse(b)
+	if err != nil {
+		t.Errorf("failed to parse: %s", err)
+	}
+
+	if msg.String() != "ID: 9071 Query qr rd ra NOERROR QD: google.com. IN A AN: google.com. IN A 205 172.217.175.110 ReqUDPSize=512" {
+		t.Errorf("failed to parse simple response, got %s", msg.String())
+	}
+
+	log.Printf("parsed: %s", msg.String())
 }
