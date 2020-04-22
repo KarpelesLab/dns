104eb6a7ca504fdfee05e0119d0ac74a3b49aef4
Mark Karpeles <magicaltux@gmail.com>
2020-04-22 14:22:10 +0900

    wip dns handler

--- dnsd/query.go
+++ dnsd/query.go
@@ -11,8 +11,15 @@ import (
 func handleQuery(pkt *dnsmsg.Message, addr net.Addr) (*dnsmsg.Message, error) {
 	log.Printf("handle query: %s", pkt)
 
-	if pkt.Bits.IsResponse() || pkt.Bits.OpCode() != dnsmsg.Query {
+	if pkt.Bits.IsResponse() || pkt.Bits.OpCode() != dnsmsg.Query || len(pkt.Question) != 1 {
 		return nil, errors.New("not a query")
 	}
-	return nil, nil
+
+	q := pkt.Question[0]
+	_ = q
+
+	pkt.Bits.SetResponse(true)
+	pkt.Bits.SetRCode(dnsmsg.ErrName)
+
+	return pkt, nil
 }
