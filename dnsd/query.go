223c2f493cc165b20e1a3da61e1a31e19b08e235
Mark Karpeles <magicaltux@gmail.com>
2020-04-22 14:31:28 +0900

    test code

--- dnsd/query.go
+++ dnsd/query.go
@@ -16,9 +16,51 @@ func handleQuery(pkt *dnsmsg.Message, addr net.Addr) (*dnsmsg.Message, error) {
 	}
 
 	q := pkt.Question[0]
-	_ = q
-
 	pkt.Bits.SetResponse(true)
+
+	// test
+	switch q.Name {
+	case "ns1.zedns.net.":
+		pkt.Bits.SetAuth(true)
+		if q.Class == dnsmsg.IN && q.Type == dnsmsg.A {
+			pkt.Answer = append(pkt.Answer, &dnsmsg.Resource{
+				Name:  "ns1.zedns.net.",
+				Class: dnsmsg.IN,
+				Type:  dnsmsg.A,
+				TTL:   86400,
+
+				Data: &dnsmsg.RDataIP{IP: net.IPv4(18, 181, 102, 53), Type: dnsmsg.A},
+			})
+		}
+		return pkt, nil
+	case "ns2.zedns.net.":
+		pkt.Bits.SetAuth(true)
+		if q.Class == dnsmsg.IN && q.Type == dnsmsg.A {
+			pkt.Answer = append(pkt.Answer, &dnsmsg.Resource{
+				Name:  "ns2.zedns.net.",
+				Class: dnsmsg.IN,
+				Type:  dnsmsg.A,
+				TTL:   86400,
+
+				Data: &dnsmsg.RDataIP{IP: net.IPv4(34, 237, 237, 237), Type: dnsmsg.A},
+			})
+		}
+		return pkt, nil
+	case "ns3.zedns.net.":
+		pkt.Bits.SetAuth(true)
+		if q.Class == dnsmsg.IN && q.Type == dnsmsg.A {
+			pkt.Answer = append(pkt.Answer, &dnsmsg.Resource{
+				Name:  "ns3.zedns.net.",
+				Class: dnsmsg.IN,
+				Type:  dnsmsg.A,
+				TTL:   86400,
+
+				Data: &dnsmsg.RDataIP{IP: net.IPv4(3, 11, 47, 103), Type: dnsmsg.A},
+			})
+		}
+		return pkt, nil
+	}
+
 	pkt.Bits.SetRCode(dnsmsg.ErrName)
 
 	return pkt, nil
